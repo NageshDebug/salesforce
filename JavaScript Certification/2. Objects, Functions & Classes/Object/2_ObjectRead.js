d30f6b12bbf756a8334a9fe10c3f98924ae11c48
Nagesh <nsiripurapu@paypal.com>
2020-12-31 22:02:28 -0800

    ES6

--- JavaScript Certification/2. Objects, Functions & Classes/Object/2_ObjectRead.js
+++ JavaScript Certification/2. Objects, Functions & Classes/Object/2_ObjectRead.js
@@ -9,11 +9,50 @@ console.log(person.firstName);
 console.log(person.lastName);
 console.log(person.age);
 
+//Problem with Method-1
+const myObject = {
+    "title": "Debug",
+    "$title": "Debug",
+    "_title": "Debug",
+    "title9": "Debug",
+    "title.name": "Debug",
+    "title-name": "Debug",
+    "5title": "Debug",
+}
+//valid identifiers
+console.log(myObject.title); //Debug
+console.log(myObject.$title); //Debug
+console.log(myObject._title); //Debug
+console.log(myObject.title9); //Debug
+
+//not valid identifiers
+console.log(myObject.title.name); //undefined
+//console.log(myObject.title-name); //NaN
+//console.log(myObject.5title); // throws SyntaxError: Unexpected number
+
 
 //Method-2
 console.log(person['firstName']);
 console.log(person['lastName']);
 console.log(person['age']);
+
+//Method-1 problem solved by Method-2
+console.log(myObject['title.name']); //Debug
+console.log(myObject['title-name']); //Debug
+console.log(myObject['5title']); //Debug
+
+//we can use this method when the property is dynamic, i.e. it can be changed during program execution.
+const myForm = {
+    input_1: "Name",
+    input_2: "Last Name",
+    input_3: "Age",
+}
+
+let size = Object.keys(myForm).length;
+for (let i = 1; i < size + 1; i++) {
+    console.log(myForm["input_" + i]);
+}
+
 
 
 //Method-3
@@ -31,3 +70,9 @@ Object.entries(person).forEach(element => console.log(person[element[0]]));
 //Method-5
 Object.values(person).forEach(value => console.log(value));
 
+
+//Method-6 Object Destructuring
+const stu = { name: 'Nagesh', age: 34, course: 'CSE' };
+const { course, age } = stu;
+console.log(course);
+console.log(age); 
