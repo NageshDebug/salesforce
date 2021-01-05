ee24da8b69f0e14e41503e23646c2ff28da97316
Nagesh <nsiripurapu@paypal.com>
2021-01-04 17:51:43 -0800

    Continuation Pattern

---
+++ force-app/main/default/lwc/Continuation Pattern/lwc/apexSimpleContinuationForLWC/apexSimpleContinuationForLWC.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+import startContinuation from '@salesforce/apexContinuation/ApexContinuation1.startConinuation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class ApexSimpleContinuationForLWC extends LightningElement {
+    result;
+    isLoading = true;
+
+    connectedCallback() {
+        startContinuation()
+            .then(result => {
+                this.result = result;
+                this.isLoading = false;
+            })
+            .catch(error => {
+                this.displayErrorToast(error.body.message);
+                this.isLoading = false;
+            });
+    }
+
+    displayErrorToast(message) {
+        const evt = new ShowToastEvent({
+            message: message,
+            variant: 'error'
+        });
+        this.dispatchEvent(evt);
+    }
+}
